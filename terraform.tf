f854566828e9baa101529b33bf7f962fce5fc4ae
Tim Beyer <tbeyer@hashicorp.com>
2023-12-01 11:01:25 -0800

    add tf config

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  cloud {
+    organization = "marsey"
+    
+    workspaces {
+      name = "eks-test-cluster"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.29.0"
+    }
+  }
+}
